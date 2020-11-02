a739137d0ee45d45d5f86bf43f6e156a29436772
Aurora Brune <akbrune@hotmail.com>
2020-11-02 12:21:04 -0600

    added components

--- portfolio/src/components/Navbar/navbar.js
+++ portfolio/src/components/Navbar/navbar.js
@@ -1,37 +1,38 @@
-import React from "react";
+// import React from "react";
+// import Navbar from "react-bootstrap/Navbar";
 
-{/*Responsive/Interractive NavBar*/ }
+// {/*Responsive/Interractive NavBar*/ }
 
-function Navbar() {
-    return (
+// function Navbar() {
+//     return (
 
-        <div class="mx-auto col-lg-12">
-            <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
-                <a class="navbar-brand">Aurora Brune</a>
-                <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+//         <div class="mx-auto col-lg-12">
+//             <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+//                 <a class="navbar-brand">Aurora Brune</a>
+//                 <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+//                     <span class="navbar-toggler-icon"></span>
+//                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                    <ul class="nav navbar-nav mr-auto">
+//                     <ul class="nav navbar-nav mr-auto">
 
-                        <li class="nav-item active">
-                            <a class="nav-link" href="./index.html">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./contact.html">Contact Info</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./portfolio.html">Portfolio</a>
-                        </li>
+//                         <li class="nav-item active">
+//                             <a class="nav-link" href="./index.html">Home <span class="sr-only">(current)</span></a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="./contact.html">Contact Info</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="./portfolio.html">Portfolio</a>
+//                         </li>
 
-                    </ul>
+//                     </ul>
 
-                </div>
-            </nav>
-        </div>
-);
-}
+//                 </div>
+//             </nav>
+//         </div>
+// );
+// }
 
-export default Navbar;
+// export default Navbar;
--- portfolio/src/components/Project/project.js
+++ portfolio/src/components/Project/project.js
@@ -1,25 +1,25 @@
-import React from "react";
-import Col from "col";
+// import React from "react";
+// import Col from "col";
 
-function Project() {
-    return (
-        <Col size="md-4 sm-12">
-            <div className="button-container">
-                <img
-                    className="img-fluid portfolio-pic"
-                    src={props.imgPath}
-                    alt={props.name}
-                />
-                <button
-                    type="button"
-                    className="btn btn-primary btn-lg btn-block deployedBtn"
-                    onClick={() => window.open(props.linkDeployed)}
-                >
-                    {props.name} (Deployed)
-                 </button>
-            </div>
-        </Col>
-    );
-}
+// function Project() {
+//     return (
+//         <Col size="md-4 sm-12">
+//             <div className="button-container">
+//                 <img
+//                     className="img-fluid portfolio-pic"
+//                     src={props.imgPath}
+//                     alt={props.name}
+//                 />
+//                 <button
+//                     type="button"
+//                     className="btn btn-primary btn-lg btn-block deployedBtn"
+//                     onClick={() => window.open(props.linkDeployed)}
+//                 >
+//                     {props.name} (Deployed)
+//                  </button>
+//             </div>
+//         </Col>
+//     );
+// }
 
-export default Project;
+// export default Project;
