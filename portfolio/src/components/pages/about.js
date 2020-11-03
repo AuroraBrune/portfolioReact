f1d73ebd87d2f10ba50d49110fafd6faf575f6c9
Aurora Brune <akbrune@hotmail.com>
2020-11-02 18:46:01 -0600

    added spacing in page

--- portfolio/src/components/pages/about.js
+++ portfolio/src/components/pages/about.js
@@ -5,24 +5,24 @@ import Img2 from "../../assets/images/Aurora-profile3.jpg";
 const About = () => {
     return (
         <div>
-            <div className="container">
-            <div className="row">
-               
-                <div className="col-lg-2">
-                    I'm Aurora Brune a Full-Stack Web Developer. I recently graduated from the
-                    University of Kansas Coding Bootcamp. I grew up as a Military dependent in a large family. My father served in
-                    the Air Force and I joined the Air Force after High School. I worked on Aircraft maintaining and
-                    troubleshooting Avionics on C-5 and C-141 aircraft, which is comparable to debugging. A problem solver, math and
-                    science person who loves the outdoors and gardening.
+            <div className="about-container">
+                <div className="row">
+             
+                    <div className="col-lg-2">
+                        I'm Aurora Brune a Full-Stack Web Developer. I recently graduated from the
+                        University of Kansas Coding Bootcamp. I grew up as a Military dependent in a large family. My father served in
+                        the Air Force and I joined the Air Force after High School. I worked on Aircraft maintaining and
+                        troubleshooting Avionics on C-5 and C-141 aircraft, which is comparable to debugging. A problem solver, math and
+                        science person who loves the outdoors and gardening.
                 </div>
-                <div className="col-lg-2"> I'm Aurora Brune a Full-Stack Web Developer. I recently graduated from the
+                    <div className="col-lg-2"> I'm Aurora Brune a Full-Stack Web Developer. I recently graduated from the
                     University of Kansas Coding Bootcamp. I grew up as a Military dependent in a large family. My father served in
                     the Air Force and I joined the Air Force after High School. I worked on Aircraft maintaining and
                     troubleshooting Avionics on C-5 and C-141 aircraft, which is comparable to debugging. A problem solver, math and
                     science person who loves the outdoors and gardening.
                 </div>
 
-                <div className="col-lg-2">Currently, a Special Education Paraprofessional for 18 years and additionally, for last
+                    <div className="col-lg-2">Currently, a Special Education Paraprofessional for 18 years and additionally, for last
                     several years, I
                     also worked a second position for the city's Parks and Recreation Department as Program Director of the After
                     School/Summer Day Camp.
@@ -33,7 +33,7 @@ const About = () => {
                     Covid-19 virus, like for many individuals, both of these positions have been severely impacted.
                 </div>
 
-                <div className="col-lg-2">This leads me to KU Coding Bootcamp program. I feel coding can tap into my existing
+                    <div className="col-lg-2">This leads me to KU Coding Bootcamp program. I feel coding can tap into my existing
                     skillsets;
                     analyzing, correcting and developing existing systems while coaching and mentoring young talent. This new career
                     opporunity allows me
@@ -43,11 +43,11 @@ const About = () => {
                     hope to work on a
                     team to share in their knowledge and responsibities. Thank you.
                 </div>
-                <div className="col-lg-2">
-                    <img src={Img2} alt="Aurora Brune" height="300px" width="225px"
-                        className="photo" />
+                    <div className="col-lg-2">
+                        <img src={Img2} alt="Aurora Brune" height="300px" width="225px"
+                            className="photo" />
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     );
