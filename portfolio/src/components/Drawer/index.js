03ecb5bfc557e5666bf6ae3cb9c8e0a8afeef062
Aurora Brune <akbrune@hotmail.com>
2021-07-01 15:16:31 -0500

    added code to toggle navbar drawer closed on click

--- portfolio/src/components/Drawer/index.js
+++ portfolio/src/components/Drawer/index.js
@@ -4,12 +4,18 @@ import { Navigation, Drawer } from "react-mdl";
 import "./style.css";
 
 function Sidebar() {
+    function hideToggle() {
+        var selectorId = document.querySelector('.mdl-layout');
+        selectorId.MaterialLayout.toggleDrawer();
+    }
+   
     return (
+        
         <Drawer className="drawer" title="Select a Page">
             <Navigation className="drawer">
-                <Link to="/">About</Link>
-                <Link to="/portfolio">Portfolio</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/" onClick={() => hideToggle()}>About</Link>
+                <Link to="/portfolio" onClick={() => hideToggle()}>Portfolio</Link>
+                <Link to="/contact" onClick={() => hideToggle()}>Contact</Link>
             </Navigation>
         </Drawer>
     )
